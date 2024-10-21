af03184a8b78cc27cb361a6292d3a4666f3b582d
Đức Quang <166306760+ndquang21@users.noreply.github.com>
2024-10-21 23:50:09 +0700

    Add files via upload

---
+++ Week 5 De Quy Quay Lui/BT_liet_ke_xau_nhi_phan_do_dai_n.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define N 21
+int X[N];
+int n;
+int check(int v, int k){
+    return 1;
+}
+
+void solution (){
+    for (int i = 1; i <= n; i++){
+        printf ("%d", X[i]);
+    }
+    printf ("\n");
+}
+
+void Try(int k){
+        for (int v = 0; v <= 1; v++){
+            if(check(v,k)){
+                X[k] = v;
+                if (k == n) solution();
+                else Try(k+1);
+            }
+        }
+}
+
+int main (){
+    scanf ("%d", &n);
+    Try(1);
+    return 0;
+}
